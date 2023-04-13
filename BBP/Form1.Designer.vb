6876c653c570c1fa605f4ad3e3d5c31f36b1deb1
Kutakazi Kanzaki <kutakazichan@gmail.com>
2023-04-14 02:02:47 +1000

    Add project files.

---
+++ BBP/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tt = New System.Windows.Forms.ToolTip(Me.components)
+        Me.btnReturn = New System.Windows.Forms.Button()
+        Me.btnInfo = New System.Windows.Forms.Button()
+        Me.btnMinimize = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.pbBar = New System.Windows.Forms.PictureBox()
+        CType(Me.pbBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tt
+        '
+        Me.tt.AutomaticDelay = 250
+        '
+        'btnReturn
+        '
+        Me.btnReturn.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.btnReturn.BackgroundImage = Global.BBP.My.Resources.Resources.Back_Grey__2_
+        Me.btnReturn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnReturn.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btnReturn.Enabled = False
+        Me.btnReturn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnReturn.Location = New System.Drawing.Point(12, 10)
+        Me.btnReturn.Name = "btnReturn"
+        Me.btnReturn.Size = New System.Drawing.Size(70, 70)
+        Me.btnReturn.TabIndex = 5
+        Me.tt.SetToolTip(Me.btnReturn, "Return")
+        Me.btnReturn.UseVisualStyleBackColor = False
+        '
+        'btnInfo
+        '
+        Me.btnInfo.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.btnInfo.BackgroundImage = Global.BBP.My.Resources.Resources.Info
+        Me.btnInfo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnInfo.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btnInfo.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnInfo.Location = New System.Drawing.Point(88, 10)
+        Me.btnInfo.Name = "btnInfo"
+        Me.btnInfo.Size = New System.Drawing.Size(70, 70)
+        Me.btnInfo.TabIndex = 4
+        Me.tt.SetToolTip(Me.btnInfo, "Information")
+        Me.btnInfo.UseVisualStyleBackColor = False
+        '
+        'btnMinimize
+        '
+        Me.btnMinimize.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.btnMinimize.BackgroundImage = Global.BBP.My.Resources.Resources.Minimize
+        Me.btnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnMinimize.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnMinimize.Location = New System.Drawing.Point(906, 10)
+        Me.btnMinimize.Name = "btnMinimize"
+        Me.btnMinimize.Size = New System.Drawing.Size(70, 70)
+        Me.btnMinimize.TabIndex = 3
+        Me.tt.SetToolTip(Me.btnMinimize, "Minimize")
+        Me.btnMinimize.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.btnExit.BackgroundImage = Global.BBP.My.Resources.Resources.Close
+        Me.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnExit.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.Location = New System.Drawing.Point(982, 10)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(70, 70)
+        Me.btnExit.TabIndex = 0
+        Me.tt.SetToolTip(Me.btnExit, "Exit")
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.BackColor = System.Drawing.SystemColors.Control
+        Me.lbl1.Font = New System.Drawing.Font("SansSerif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.lbl1.Location = New System.Drawing.Point(371, 27)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(338, 37)
+        Me.lbl1.TabIndex = 2
+        Me.lbl1.Text = "Backpacker Program"
+        '
+        'pbBar
+        '
+        Me.pbBar.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.pbBar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pbBar.Location = New System.Drawing.Point(0, 0)
+        Me.pbBar.Name = "pbBar"
+        Me.pbBar.Size = New System.Drawing.Size(1064, 90)
+        Me.pbBar.TabIndex = 1
+        Me.pbBar.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(1064, 681)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btnReturn)
+        Me.Controls.Add(Me.btnInfo)
+        Me.Controls.Add(Me.btnMinimize)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.pbBar)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "Form1"
+        CType(Me.pbBar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExit As Button
+    Friend WithEvents tt As ToolTip
+    Friend WithEvents pbBar As PictureBox
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents btnMinimize As Button
+    Friend WithEvents btnInfo As Button
+    Friend WithEvents btnReturn As Button
+End Class
---
+++ BBP/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+    Private Sub Button_Control(sender As Object, e As EventArgs) Handles btnExit.Click, btnInfo.Click, btnMinimize.Click
+        Select Case sender.name
+            Case "btnExit"
+                If MsgBox("Are you sure you want to exit?", vbOKCancel + vbDefaultButton2, "Confirm Exit") = vbOK Then
+                    End
+                End If
+            Case "btnInfo"
+                Dim form2 As New Form2
+                form2.Show()
+            Case "btnMinimize"
+                Me.WindowState = FormWindowState.Minimized
+        End Select
+    End Sub
+
+    Private Sub btnMinimize_Click(sender As Object, e As EventArgs) Handles btnMinimize.Click
+
+    End Sub
+End Class
